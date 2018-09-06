4f4165bbdc5850d4079fa70697a0a5486827d90f
rsambouncer <rsambouncer@gmail.com>
2018-09-06 13:29:36 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const PORT = process.env.PORT || 3000;
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
-}).listen(8080);
+}).listen(PORT);
