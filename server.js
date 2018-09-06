5039222115f5755ad3db2c7fb07f1323e69d7eb9
rsambouncer <rsambouncer@gmail.com>
2018-09-06 13:18:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var http = require('http');
+const http = require('http');
+const PORT = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
