ad2aa2885fbf6a1dcd831721f3afcd9bd5800789
rsambouncer <rsambouncer@gmail.com>
2018-09-13 13:12:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,34 +5,9 @@ const PORT = process.env.PORT || 3000;
 http.createServer(function (req, res) {
     
     let request = url.parse(req.url);
-    let options = {
-        hostname:    "www.cheese.com",
-        port:    80,
-        path:    "/comte",
-        headers: req.headers,
-    };
     
     console.log(req.headers);
-    /*
-    let backend_req = http.request(options, function(backend_res) {
-        res.writeHead(backend_res.statusCode, backend_res.headers);
-        console.log(backend_res.headers);
-        backend_res.on('data', function(chunk) {
-            console.log(chunk);
-            res.write(chunk);
-        });
-        backend_res.on('end', function() {
-            res.end();
-        });
-    });
 
-    req.on('data', function(chunk) {
-        backend_req.write(chunk);
-    });
-
-    req.on('end', function(chunk) {
-        backend_req.end(chunk);
-    });*/
     
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('Hello World!');
