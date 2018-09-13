4613651d4fc0f36caba823eb68276840db59d693
rsambouncer <rsambouncer@gmail.com>
2018-09-13 13:47:05 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const http = require('http');
-const url  = require('url');
 const PORT = process.env.PORT || 3000;
 
-http.createServer(function (req, res) {
-    
-    let request = url.parse(req.url);
-    
-    console.log(req.headers);
+http.createServer(onClientRequest).listen(PORT);
 
+function onClientRequest(client_req, client_res){
     
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World!');
-    res.end();
-    
-}).listen(PORT);
+    console.log(client_req.headers);
+    let options = {
+            hostname: 'rsambouncer.neocities.org',
+            port: 80,
+            path: '/index.html',
+            method: 'GET'
+        };
+    let server_req = http.request(options, function(server_res){
+        let body = "";
+        server_res.on('data', function(chunk){
+            body+=chunk;
+        });
+        server_res.on('end',function(){
+            client_res.writeHead(server_res.statusCode, server_res.headers);
+            console.log(body);
+            client_res.end(body);
+        });
+    }
+}
+
+
+
+
+
