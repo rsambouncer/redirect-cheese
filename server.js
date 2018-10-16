6c08c9e325aade5d506a132a5a098691b7c884fe
rsambouncer <rsambouncer@gmail.com>
2018-10-16 11:00:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -27,8 +27,7 @@ function httpsReqFromURL(requrl, client_req, client_res){
     
     let qobj = url.parse(requrl);
     if(!qobj.hostname){
-        if(client_req.headers['authorization']!=="Basic Y2hlZXNlOmNoZWVzZQ=="){ 
-            console.log(client_req.headers['authorization']);
+        if(client_req.headers['authorization']!=="Basic Y2hlZXNlOmNoZWVzZQ=="){ //cheese:cheese
             client_res.writeHead(401, {"WWW-Authenticate": "Basic"});
             client_res.end("not authenticated");
         }else{
