383323062ddd5063aaa427f87308f3ed7aeae858
rsambouncer <rsambouncer@gmail.com>
2019-02-16 15:37:03 -0800

    Update proxy2.js

--- proxy2.js
+++ proxy2.js
@@ -4,6 +4,8 @@ const proxy     = httpProxy.createProxyServer({});
 const PORT = process.env.PORT || 3000;
 
 http.createServer(function(req, res) {
+  console.log(req.url);
+  if(req.url==="/") res.end("Hi");
   proxy.web(req, res, { target: req.url });
 }).listen(PORT, () => {
   console.log("Waiting for requests on port "+PORT);
