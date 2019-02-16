1f2ac64e70c68dce6d9614d04973ac1bbc2c13e7
rsambouncer <rsambouncer@gmail.com>
2019-02-16 14:59:06 -0800

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -7,11 +7,36 @@ http.createServer(onClientRequest).listen(PORT);
 
 function onClientRequest(client_req, client_res){
     
-    console.log(client_req.url);
+    console.log("Getting resource: "+ client_req.url);
     
-    let requestedurl = url.parse(client_req.url).path.substring(1);
+    let qobj = url.parse(client_req.url);
+        if(!qobj.hostname) client_res.end("Request not formatted correctly: No hostname");
+    let options = {
+        protocol: qobj.protocol||"https:",
+        hostname: qobj.hostname, 
+        headers:  client_req.headers};
+        if(qobj.auth) options.auth = qobj.auth;
+        if(qobj.port) options.port = qobj.port;
+        if(qobj.method) options.method = qobj.method;
+        if(qobj.path) options.path = qobj.path;
     
-    client_res.end("Hello there: \n"+client_req.url);
-    
+    let proto = options.protocol==="https:"?https:http;
+    let server_req = proto.request(options, function(server_res){
+        let body = "";
+        server_res.on('data', function(chunk){
+            body+=chunk;
+        });
+        server_res.on('end', function(){
+            client_res.writeHead(200, server_res.headers);
+            client_res.end(body);
+        });
+        server_res.on('error',function(err){
+            console.log(err);
+        });
+    });
+        
+    client_req.on('data', (chunk)=>{server_req.write(chunk);console.log("chunk: "+chunk);} );
+    client_req.on('error', (err)=>{console.log(err);} );
+    client_req.on('end', ()=>{ server_req.end();} );
 }
 
