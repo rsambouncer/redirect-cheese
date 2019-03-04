2931e52af20922100c7fd36bd002a7e049b26dae
rsambouncer <rsambouncer@gmail.com>
2019-03-04 00:39:01 -0800

    Update nodeproxy.js

--- nodeproxy.js
+++ nodeproxy.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const net = require('net');
 const url = require('url');
+const PORT = 8080;
 
 // create an HTTP tunneling proxy
 const proxy = http.createServer((req, res) => {
@@ -10,16 +11,18 @@ const proxy = http.createServer((req, res) => {
     method:req.method
   };
   let newreq = http.request(req.url,options, (newres)=>{
+    res.writeHead(newres.statusCode, newres.headers);
     newres.pipe(res);
   });
   req.pipe(newreq);
+  newreq.on('error',(e)=>{console.log("Client server error: "+e.message);});
 });
 
 //HTTPS requests send a connect
 //connect handler code from https://nodejs.org/api/http.html#http_event_connect
 proxy.on('connect', (req, cltSocket, head) => {
   console.log("Connect Proxying "+req.url);
-  const srvUrl = url.parse(`http://${req.url}`);
+  const srvUrl = url.parse("http://"+req.url);
   const srvSocket = net.connect(srvUrl.port, srvUrl.hostname, () => {
     cltSocket.write('HTTP/1.1 200 Connection Established\r\n' +
                     'Proxy-agent: Node.js-Proxy\r\n' +
@@ -34,7 +37,7 @@ proxy.on('connect', (req, cltSocket, head) => {
 
 // now that proxy is running
 proxy.on('error',(e)=>{console.log("Proxy server error: "+e.message);});
-proxy.listen(8080, () => {
-  console.log("now listening");
+proxy.listen(PORT, () => {
+  console.log("now listening on port "+PORT);
 });
 
